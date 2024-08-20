a66599436f52623d844636e260d2131e633c4019
Nadinda <nadinda@nadinda.dev>
2024-08-20 17:55:57 +1000

    feat: create header with logo and title

--- src/Main.elm
+++ src/Main.elm
@@ -2,16 +2,21 @@ module Main exposing (main, update, view)
 
 import Browser
 import Grid exposing (viewGrid)
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, h1, header, img, text)
+import Html.Attributes exposing (alt, class, src)
 import Html.Events exposing (onClick)
 import Robot exposing (Model, Msg(..), initModel, moveTo, rotateToLeft, rotateToRight)
+import VitePluginHelper exposing (asset)
 
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
-        [ div [ class "grid" ] (viewGrid model)
+        [ header [ class "header" ]
+            [ img [ src <| asset "/src/assets/logo.png", alt "Logo" ] []
+            , h1 [ class "title" ] [ text "Elmroybot" ]
+            ]
+        , div [ class "grid" ] (viewGrid model)
         , div [ class "btn-group" ]
             [ button [ onClick RotateLeft ] [ text "Rotate ↺" ]
             , button [ onClick MoveForward ] [ text "Go Forward" ]
